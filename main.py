994679d31cdf018cac4058ddb63ffda2f8a74003
hisyamzayd <hisyamzayd@gmail.com>
2020-05-11 13:38:19 +0700

    main.py : change variable name. readme.md : add algorithm step and symbol legends

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-data_latih = [
+x = [
     [5.1, 3.5, 1.4, 0.2], 
     [6.2, 2.2, 4.5, 1.5], 
     [7.7, 2.8, 6.7, 2.0]
 ]
 
-target_data_latih = [
+t = [
     [1, 0, 0], #iris-setosa
     [0, 1, 0], #iris-versicolor
     [0, 0, 1]  #iris-virginica
@@ -17,15 +17,15 @@ v_i_j = [
     [0.2, -0.1, -0.2, 0.3]
 ]
 
-bias_hidden = [0.25, 0.45]
+v_0_j = [0.25, 0.45]
 
-w = [
+w_j_k = [
     [-0.1, 0.2],
     [0.3, -0.2],
     [-0.4, 0.1]
 ]
 
-bias_output = [-0.4, 0.2, 0.1]
+w_0_k = [-0.4, 0.2, 0.1]
 
 alpha = 0.1
 
@@ -60,7 +60,7 @@ def bobot_nguyen_widrow(n, p, v_i_j) :
         v_i_j_baru.append(each)
     return v_i_j_baru
 
-def each_feedforward(v_i_j_baru, each_data_latih, bias_hidden, w, bias_output) :
+def each_feedforward(v_i_j_baru, each_data_latih, v_0_j, w_j_k, w_0_k) :
     # perhitungan z
     z_in = []
     for i in range(len(v_i_j_baru)) :
@@ -68,22 +68,22 @@ def each_feedforward(v_i_j_baru, each_data_latih, bias_hidden, w, bias_output) :
         for j in range(len(v_i_j_baru[0])) : 
             multi = v_i_j_baru[i][j] * each_data_latih[j]
             each_z_in += multi
-        each_z_in += bias_hidden[i]
+        each_z_in += v_0_j[i]
         z_in.append(each_z_in)
     z = [fungsi_aktivasi(i) for i in z_in]
     # perhitungan y
     y_in = []
-    for i in range(len(w)) :
+    for i in range(len(w_j_k)) :
         each_y_in = 0
-        for j in range(len(w[0])) :
-            multi = w[i][j] * z[j]
+        for j in range(len(w_j_k[0])) :
+            multi = w_j_k[i][j] * z[j]
             each_y_in += multi
-        each_y_in += bias_output[i]
+        each_y_in += w_0_k[i]
         y_in.append(each_y_in)
     y = [fungsi_aktivasi(i) for i in y_in]
     return z, z_in, y, y_in
 
-def each_backpropagation(each_data_latih, each_target_data_latih, w, alpha, z, z_in, y, y_in) :
+def each_backpropagation(each_data_latih, each_target_data_latih, w_j_k, alpha, z, z_in, y, y_in) :
     ### perhitungan bobot output layer
     # 1. hitung t-y
     t_min_y = []
@@ -107,8 +107,8 @@ def each_backpropagation(each_data_latih, each_target_data_latih, w, alpha, z, z
     Δw_0_k = [alpha * i for i in δk]
     
     ### perhitungan bobot hidden layer
-    # do transpose with w untuk mempermudah perhitungan
-    w_transpose = np.transpose(w)
+    # do transpose with w_j_k untuk mempermudah perhitungan
+    w_transpose = np.transpose(w_j_k)
 
     # 1. perhitungan delta kecil in j (seperti t-y tapi di hidden layer)
     δin_j = []
@@ -151,34 +151,36 @@ def update_bobot_func(matrix1, matrix2) :
             result[i] = matrix1[i] + matrix2[i]
     return result
 
-def each_update_bobot(w, bias_output, v_i_j_baru, bias_hidden, Δw_j_k, Δw_0_k, Δv_i_j, Δv_0_j) :
-    w_baru = update_bobot_func(w, Δw_j_k)
-    bias_output_baru = update_bobot_func(bias_output, Δw_0_k)
+def each_update_bobot(w_j_k, w_0_k, v_i_j_baru, v_0_j, Δw_j_k, Δw_0_k, Δv_i_j, Δv_0_j) :
+    w_baru = update_bobot_func(w_j_k, Δw_j_k)
+    bias_output_baru = update_bobot_func(w_0_k, Δw_0_k)
     v_i_j_baru = update_bobot_func(v_i_j_baru, Δv_i_j)
-    bias_hidden_baru = update_bobot_func(bias_hidden, Δv_0_j)
+    bias_hidden_baru = update_bobot_func(v_0_j, Δv_0_j)
 
     return w_baru, bias_output_baru, v_i_j_baru, bias_hidden_baru
 
 ### PERHITUNGAN KEDUA
-def pengujian(v_i_j_baru, data_latih, target_data_latih, bias_hidden, w, bias_output, alpha, max_epoch) :
+def perhitungan_bobot(v_i_j_baru, x, t, v_0_j, w_j_k, w_0_k, alpha, max_epoch) :
     epoch = 0
     while (epoch < max_epoch) :
         epoch += 1
-        z, z_in, y, y_in = each_feedforward(v_i_j_baru, data_latih[0], bias_hidden, w, bias_output)
-        Δw_j_k, Δw_0_k, Δv_i_j, Δv_0_j = each_backpropagation(data_latih[0], target_data_latih[0], w, alpha, z, z_in, y, y_in)
-        w, bias_output, v_i_j_baru, bias_hidden = each_update_bobot(w, bias_output, v_i_j_baru, bias_hidden, Δw_j_k, Δw_0_k, Δv_i_j, Δv_0_j)
-        print('epoch ke ', epoch)
+        z, z_in, y, y_in = each_feedforward(v_i_j_baru, x[0], v_0_j, w_j_k, w_0_k)
+        Δw_j_k, Δw_0_k, Δv_i_j, Δv_0_j = each_backpropagation(x[0], t[0], w_j_k, alpha, z, z_in, y, y_in)
+        w_j_k, w_0_k, v_i_j_baru, v_0_j = each_update_bobot(w_j_k, w_0_k, v_i_j_baru, v_0_j, Δw_j_k, Δw_0_k, Δv_i_j, Δv_0_j)
+        print('epoch - ', epoch)
     print()
-    return w, bias_output, v_i_j_baru, bias_hidden
+    return w_j_k, w_0_k, v_i_j_baru, v_0_j
 
 # Main Function
 if __name__ == "__main__" :
 
-    v_i_j_baru = bobot_nguyen_widrow(len(data_latih[0]), len(target_data_latih), v_i_j)
+    # inisialisasi bobot
+    v_i_j_baru = bobot_nguyen_widrow(len(x[0]), len(t), v_i_j)
 
-    w, bias_output, v_i_j_baru, bias_hidden = pengujian(v_i_j_baru, data_latih, target_data_latih, bias_hidden, w, bias_output, alpha, max_epoch)
+    # mulai perhitungan bobot
+    w_j_k, w_0_k, v_i_j_baru, v_0_j = perhitungan_bobot(v_i_j_baru, x, t, v_0_j, w_j_k, w_0_k, alpha, max_epoch)
 
-    print('bobot output = \n', w, '\n')
-    print('bias output = \n', bias_output, '\n')
-    print('bobot hidden = \n', v_i_j_baru, '\n')
-    print('bias hidden = \n', bias_hidden)
+    print('bobot output (w_j_k) = \n', w_j_k, '\n')
+    print('bias output (w_0_k) = \n', w_0_k, '\n')
+    print('bobot hidden (v_i_j) = \n', v_i_j_baru, '\n')
+    print('bias hidden (v_0_j) = \n', v_0_j)
--- readme.md
+++ readme.md
@@ -1,7 +1,6 @@
 # Backpropagation Neural Network with Iris Dataset
 
 ## Iris Dataset
-
 |  Petal Length |  Petal Width |  Sepal Length |  Sepal Width |      Class      |
 |:-------------:|:------------:|:-------------:|:------------:|:---------------:|
 |      5.1      |      3.5     |      1.4      |      0.2     |   iris-setosa   |
@@ -9,7 +8,6 @@
 |      7.7      |      2.8     |      6.7      |      2.0     |  iris-virginica |
 
 ## Activation Function
-
 Using binary sigmoid function
 
 ![\f(x) = \frac{1}{1+e^-x}](https://latex.codecogs.com/svg.latex?f%28x%29%20%3D%20%5Cfrac%7B1%7D%7B1&plus;e%5E-x%7D)
@@ -31,7 +29,6 @@ def fungsi_aktivasi_turunan(x) :
 ```
 
 ## Stop Condition
-
 Using epoch. Max epoch = 1100
 
 ```python
@@ -39,5 +36,44 @@ while (epoch < max_epoch) :
 ```
 
 ## How to Run
-
 Install Python 3.x -> Install Numpy at Python -> Use whatever IDE then RUN main.py
+
+## Tahap Perhitungan Algoritma Pengujian
+0. Inisialisasi data latih, bobot awal, bias awal semua layer semua neuron
+1. Inisialisasi bobot Nguyen-Widrow (untuk bobot hidden saja)
+2. Feed forward dari masing-masing row data latih
+3. Fase distribusi error
+4. Fase update bobot
+5. Lakukan iterasi sampai memenuhi syarat tertentu (max epoch)
+
+## LEGENDS
+# 0 dan 1
+n           = panjang kolom data latih (banyak fitur)
+p           = panjang row target data latih (banyak data latih)
+x           = data latih
+t           = target data latih
+v_i_j       = bobot awal untuk hidden neuron (acak)
+v_i_j_baru  = bobot baru untuk hidden neuron (Nguyen-Widrow)
+v_0_j       = bias awal untuk hidden neuron (acak)
+w_j_k       = bobot awal untuk output neuron (acak)
+w_0_k       = bias awal untuk output neuron (acak)
+
+# 2
+z_in         = hasil perhitungan input pada hidden neuron
+z           = hasil perhitungan output (z_in) hidden neuron dengan f(x)
+y_in        = hasil perhitungan input pada output neuron
+y           = hasil perhitungan output (y_in) hidden neuron dengan f(x)
+
+# 3
+δk          = faktor error pada output layer
+δin_j       = hasil jumlah delta error dari output neuron (menggunakan δk)
+δj          = faktor error pada hidden layer
+
+# 4
+Δw_j_k      = perubahan bobot pada output layer (menggunakan δk)
+Δw_0_k      = perubahan bias pada output layer (menggunakan δk)
+Δv_i_j      = perubahan bobot pada hidden layer (menggunakan δj)
+Δv_0_j      = perubahan bias pada hidden layer (menggunakan δj)
+
+# 5
+epoch       = genap 1 kali iterasi dari semua data latih
